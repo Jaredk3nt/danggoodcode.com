03f9db769725664c036a26ad824c542a420a0957
Jared Jones <jkjones.dev@gmail.com>
2026-07-27 19:05:51 -0700

    Adding project settings

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
-const generatePathMap = require('./build/generatePathMap');
+// @ts-nocheck
+module.exports = function(...args) {
+  let original = require('./next.config.__vercel_builder_backup__.js');
 
-module.exports = {
-  exportTrailingSlash: true,
-  exportPathMap: generatePathMap
+  const finalConfig = {};
+  const target = { target: 'serverless' };
+
+  if (typeof original === 'function' && original.constructor.name === 'AsyncFunction') {
+    // AsyncFunctions will become promises
+    original = original(...args);
+  }
+
+  if (original instanceof Promise) {
+    // Special case for promises, as it's currently not supported
+    // and will just error later on
+    return original
+      .then((orignalConfig) => Object.assign(finalConfig, orignalConfig))
+      .then((config) => Object.assign(config, target));
+  } else if (typeof original === 'function') {
+    Object.assign(finalConfig, original(...args));
+  } else if (typeof original === 'object') {
+    Object.assign(finalConfig, original);
+  }
+
+  Object.assign(finalConfig, target);
+
+  return finalConfig;
 }
